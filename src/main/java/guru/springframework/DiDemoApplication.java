f28f63c794587a9a4b8a66f5da51c7c7dac1d152
Melissa de Miranda <melissa.miranda@mercadolivre.com>
2020-01-30 08:44:11 -0300

    Spring - Profiles - @Profile

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -7,6 +7,7 @@ import guru.springframework.controllers.PropertyInjectedController;
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
 import guru.springframework.services.PrimaryGreetingService;
+import guru.springframework.services.PrimarySpanishGreetingService;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,12 +20,10 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
 		MyController controller = (MyController) ctx.getBean("myController"); //O nome do bean deve estar em minusculo
-		controller.hello();
 
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello() + " PropertyInjectedController");
 		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello() + " GetterInjectedController");
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello() + " ConstructorInjectedController");
-		//System.out.println(ctx.getBean(PrimaryGreetingService.class).sayGreeting() + " PrimaryGreetingService");
 	}
 }
