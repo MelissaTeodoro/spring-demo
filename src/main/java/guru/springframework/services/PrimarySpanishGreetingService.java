be22c6e2d5271fcf6981835bef344023e0a2a6b9
Melissa de Miranda <melissa.miranda@mercadolivre.com>
2020-02-01 17:45:19 -0300

    Ciclo de vida de um Bean

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -9,6 +9,12 @@ import org.springframework.stereotype.Service;
 @Profile("es")
 public class PrimarySpanishGreetingService implements GreetingService {
 
+    private GreetingRepository greetingRepository;
+
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hola - Servico de saludo primario";
