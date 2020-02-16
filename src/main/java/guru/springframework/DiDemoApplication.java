2bb178dd944be080fbf85f46d7c0b64ec4c8270a
Melissa de Miranda <melissa.miranda@mercadolivre.com>
2020-02-16 17:27:46 -0300

    Utilizando Property Source

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,9 +1,7 @@
 package guru.springframework;
 
-import guru.springframework.controllers.ConstructorInjectedController;
-import guru.springframework.controllers.GetterInjectedController;
 import guru.springframework.controllers.MyController;
-import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,10 +16,8 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController"); //O nome do bean deve estar em minusculo
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello() + " PropertyInjectedController");
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello() + " GetterInjectedController");
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello() + " ConstructorInjectedController");
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
 	}
 }
 
